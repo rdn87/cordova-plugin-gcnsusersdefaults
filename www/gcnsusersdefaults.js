421c8b890d77e31332604b609197c6b7ce1b8fb0
Giulio Caruso <giuliocaruso@hotmail.com>
2017-05-10 10:23:15 +0200

    Fix Params

--- www/gcnsusersdefaults.js
+++ www/gcnsusersdefaults.js
@@ -3,43 +3,43 @@ var cordova = require('cordova');
 function GCNSUsersDefaults() { }
 
 GCNSUsersDefaults.prototype.saveBoolByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', data);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.getBoolByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', data);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.saveIntByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', data);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.getIntByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', data);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.saveStringByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', data);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.getStringByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', data);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.saveDoubleByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', data);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.getDoubleByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', data);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.saveFloatByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', data);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.getFloatByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', data);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', [data]);
 };
 
 // Register the plugin
