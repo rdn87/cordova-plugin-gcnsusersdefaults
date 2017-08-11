baf468d4162148bd132adbb078e74c329aa15849
Malone, Adam (MentorMate) <adam.malone@mentormate.com>
2017-08-11 16:37:08 -0500

    add support for reading and saving Array types.  Fix javascript code to use the correct native functions for each data type

--- www/gcnsusersdefaults.js
+++ www/gcnsusersdefaults.js
@@ -11,35 +11,43 @@ GCNSUsersDefaults.prototype.getBoolByKey = function (onSuccess, onFail, data) {
 };
 
 GCNSUsersDefaults.prototype.saveIntByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', [data]);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveIntByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.getIntByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', [data]);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getIntByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.saveStringByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', [data]);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveStringByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.getStringByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', [data]);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getStringByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.saveDoubleByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', [data]);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveDoubleByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.getDoubleByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', [data]);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getDoubleByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.saveFloatByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveBoolByKey', [data]);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveFloatByKey', [data]);
 };
 
 GCNSUsersDefaults.prototype.getFloatByKey = function (onSuccess, onFail, data) {
-	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getBoolByKey', [data]);
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getFloatByKey', [data]);
+};
+
+GCNSUsersDefaults.prototype.getArrayByKey = function (onSuccess, onFail, data) {
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'getArrayByKey', [data]);
+};
+
+GCNSUsersDefaults.prototype.saveArrayByKey = function (onSuccess, onFail, data) {
+	cordova.exec(onSuccess, onFail, 'GCNSUsersDefaults', 'saveArrayByKey', [data]);
 };
 
 // Register the plugin
